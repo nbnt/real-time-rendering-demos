5bc2745227c1416cc17b11eb94465aa6c8ce7b1d
satanir <satanir@ec089ae4-f00d-54cd-c0c8-2f2296ec1a38>
2013-02-25 20:38:20 +0000

    Added movement demo
    git-svn-id: https://real-time-rendering-demos.googlecode.com/svn/trunk@10 ec089ae4-f00d-54cd-c0c8-2f2296ec1a38

--- Source/Lib/assimp/code/AssimpPCH.h
+++ Source/Lib/assimp/code/AssimpPCH.h
@@ -117,6 +117,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <numeric>
 #include <new>
 #include <cstdio>
+#include <xfunctional>
 
 // Boost headers
 #include <boost/pointer_cast.hpp>
